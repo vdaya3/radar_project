449a19dcac26d8bac0547100318fb2b20b284f62
mk1994 <murugeshk252@gmail.com>
2024-10-05 19:14:31 +0530

    initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,94 @@
+const { merge } = require('webpack-merge')
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const postcssPresetEnv = require('postcss-preset-env')
+const cssnano = require('cssnano')
+const path = require('path')
+
+const common = require('./webpack.common.js')
+const config = require('./src/config')
+const { graphConfig, uiConfig } = require('./src/graphing/config')
+
+const featureToggles = config().development.featureToggles
+const main = ['./src/site.js']
+const scssVariables = []
+
+Object.entries(graphConfig).forEach(function ([key, value]) {
+    scssVariables.push(`$${key}: ${value}px;`)
+})
+
+Object.entries(uiConfig).forEach(function ([key, value]) {
+    scssVariables.push(`$${key}: ${value}px;`)
+})
+
+Object.entries(featureToggles).forEach(function ([key, value]) {
+    scssVariables.push(`$${key}: ${value};`)
+})
+
+module.exports = merge(common, {
+    mode: 'development',
+    entry: { main: main },
+    performance: {
+        hints: false,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: { importLoaders: 1, modules: 'global', url: false },
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    postcssPresetEnv({
+                                        browsers: 'last 2 versions',
+                                        stage: 1,
+                                    }),
+                                    cssnano({
+                                        preset: ['default', { discardComments: { removeAll: true } }],
+                                    }),
+                                ],
+                            },
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            additionalData: scssVariables.join('\n'),
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.ENVIRONMENT': JSON.stringify('development'),
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        }),
+    ],
+    devtool: 'source-map',
+    watchOptions: {
+        aggregateTimeout: 300,
+        poll: 1000,
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'), // Adjust as necessary
+        },
+        proxy: {
+            '/upload': 'http://localhost:3000', // Proxy to your Express server
+        },
+        port: 8080, // Change the port if necessary
+    },
+})
